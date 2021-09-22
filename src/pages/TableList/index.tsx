35515dae3ff34dfb57880de23864942af9353147
hang.gao <yam51816@adobe.com>
2021-09-22 20:54:56 +0800

    feat: add dependencies d3.js

--- src/pages/TableList/index.tsx
+++ src/pages/TableList/index.tsx
@@ -11,6 +11,7 @@ import ProDescriptions from '@ant-design/pro-descriptions';
 import type { FormValueType } from './components/UpdateForm';
 import UpdateForm from './components/UpdateForm';
 import { rule, addRule, updateRule, removeRule } from '@/services/ant-design-pro/api';
+// import { json, csv, timeFormat, timeParse, timeMonth, timeWeek, format } from "d3";
 
 /**
  * @en-US Add node
